45d2f66cd23bbc7f4cb431c4d0ce76ad097bd71e
HoangTruongSoftDev <116051667+HoangTruongSoftDev@users.noreply.github.com>
2024-02-12 00:14:55 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -21,6 +21,15 @@ namespace Ex9
     ***
     ****
     *****
+
+     specific input is: 5 // Advanced Exercise
+
+* * * * * * * * * *
+* * * *     * * * *
+* * *         * * *
+* *             * *
+*                 *
+ 
      */
     class Program
     {
@@ -91,13 +100,54 @@ namespace Ex9
             printStarOnly(space - 1);
         }
         #endregion
+static int counter = 0; // Advanced Exercise
 
+// Advanced Exercise
+ static void DisplayPattern(int line, int index1, int index2) 
+ {
+     if (counter==line)
+     return;
+     if (counter == 0)
+     {
+         for (int i = 0; i < line*2; i++)
+             Console.Write("* ");
+         Console.WriteLine();
+         counter++;
+         DisplayPattern(line,index1,index2+1);
+         
+
+     }
+     else
+     {
+         for (int i = 1; i <= 2 * line; i++)
+         {
+             if (i >= index1 && i <= index2)
+             {
+                 Console.Write("  ");
+             }
+             else
+             {
+                 Console.Write("* ");
+             }
+             
+         }
+         Console.WriteLine();
+         counter++;
+         DisplayPattern(line,index1 - 1, index2 + 1);
+     }
+     
+ }
         static void Main(string[] args)
         {
             Console.WriteLine("PLease enter the line: ");
             int n = Convert.ToInt32(Console.ReadLine());
             //PrintPatternDown(n,"");
             printProtocole(n, 1);
+
+            // Advanced Exercise
+            Console.WriteLine("How many lines you want to input? ");
+            int line = Convert.ToInt32(Console.ReadLine());
+            DisplayPattern(line, line, line);
             Console.ReadKey();
 
         }
